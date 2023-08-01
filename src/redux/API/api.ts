a139502ec9df30077eee9ab23ff0a75d016b24ae
Marilisk <6868221@gmail.com>
2023-08-01 12:25:41 +0300

    new api

--- src/redux/API/api.ts
+++ src/redux/API/api.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const CLIENT_URL = 'https://spboptis.ru'
 export const DEV_API_URL = 'http://localhost:5555'
+export const RENDER_PROD_API_URL = 'https://backend-optics-without-packlo.onrender.com'
 export const PROD_API_URL = 'https://api.spboptis.ru'
-export const API_URL = PROD_API_URL
+export const API_URL = RENDER_PROD_API_URL
 
 const instance = axios.create({  
     baseURL: API_URL,  
